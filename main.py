20cbcf35c1e3e0ada8def496c8c11d1529ecd172
HiroshiARAKI <araki@hirab.net>
2019-10-10 14:54:35 +0900

    main.py update

--- main.py
+++ main.py
@@ -2,16 +2,23 @@ from snnlib import Spiking
 
 if __name__ == '__main__':
     # SNN構築　入力層ニューロンの数，シミュレーション時間などを決める
-    snn = Spiking(input_l=784, obs_time=100)
+    snn = Spiking(input_l=784, obs_time=300)
 
     # レイヤーを追加　数とニューロンモデルを指定する
-    snn.add_layer(n=1000, node=snn.LIF)
+    snn.add_layer(n=500, node=snn.LIF)
 
     # データセットの選択
-    snn.load_MNIST()
+    snn.load_MNIST(batch=10)
+
+    # weight mapを描画
+    for i in range(3):
+        snn.plot_output_weights_map(index=i, save=True, file_name='pre_wmp_'+str(i)+'.png')
 
     # データを順伝播させる
-    snn.run()
+    snn.run(tr_size=1000)
+
+    for i in range(3):
+        snn.plot_output_weights_map(index=i, save=True, file_name='post_wmp_'+str(i)+'.png')
 
     # 各種結果のプロット
     snn.plot_out_voltage(1, save=True)
